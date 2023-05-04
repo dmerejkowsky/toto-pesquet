6e6f7f2d0341c1aeef1bd20635c79c475f914721
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-05-04 15:59:46 +0200

    Rewrite map.test using test.each()
    Still failing for some strange reason ...

--- ts/tests/map.test.ts
+++ ts/tests/map.test.ts
@@ -2,39 +2,34 @@ import { describe, test, expect } from '@jest/globals'
 
 import { Coordinates, Direction, Orientation, Rotation, rotate, translate } from '../map'
 
-type rotationTest = [Orientation, Rotation, Orientation]
-
+type rotationTest = [[Orientation, Rotation], Orientation]
 const rotationTests: rotationTest[] = [
-  [Orientation.North, Rotation.Right, Orientation.East],
-  [Orientation.East, Rotation.Left, Orientation.North]
+  [[Orientation.North, Rotation.Right], Orientation.East],
+  [[Orientation.East, Rotation.Left], Orientation.North]
 ]
 
-for (const [orientation, rotation, expected] of rotationTests) {
-  test(`${orientation} rotating ${rotation} gives ${expected}`, t => {
-
+describe('rotations', () => {
+  test.each(rotationTests)("Rotating from %s gives %s", ([orientation, rotation], expected) => {
     const actual = rotate(orientation, rotation)
-
     expect(actual).toEqual(expected)
   })
-}
+})
 
-type translateTest = [[number, number], Orientation, Direction, [number, number]]
 
+type translateTest = [[number, number, Orientation, Direction], [number, number]]
 const translateTests: translateTest[] =
   [
-    [[0, 0], Orientation.North, Direction.Forward, [0, 1]],
-    [[0, 0], Orientation.East, Direction.Forward, [1, 0]],
-    [[0, 3], Orientation.North, Direction.Backward, [0, 2]]
+    [[0, 0, Orientation.North, Direction.Forward], [0, 1]],
+    [[0, 0, Orientation.East, Direction.Forward], [1, 0]],
+    [[0, 3, Orientation.North, Direction.Backward], [0, 2]]
   ]
 
-for (const [input, orientation, direction, expected] of translateTests) {
-  test(`translate ${input} moving ${direction} facing ${orientation} gives ${expected}`, t => {
-
-    const startCoordinates = new Coordinates(input[0], input[1])
+describe('translate', () => {
+  test.each(translateTests)("Translating %s -> %s", (input, expected) => {
+    const [x, y, orientation, direction] = input
+    const coordinates = new Coordinates(x, y)
+    const actualCoordinates = translate(coordinates, orientation, direction)
     const expectedCoordinates = new Coordinates(expected[0], expected[1])
-
-    const actualCoordinates = translate(startCoordinates, orientation, direction)
-
     expect(actualCoordinates).toEqual(expectedCoordinates)
   })
-}
+})
